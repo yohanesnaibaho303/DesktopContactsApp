50e2d5f534e4eae0ab2a9a0a2ba2b55d62222f04
yohanesnaibaho303 <yohanespratama303@gmail.com>
2024-09-16 17:44:00 +0700

    added listview

--- DesktopContactsApp/MainWindow.xaml.cs
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -39,10 +39,25 @@ namespace DesktopContactsApp
 
         void ReadDatabase()
         {
+            List<Contact> contacts;
             using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
             {
                 conn.CreateTable<Contact>();
-                var contacts = conn.Table<Contact>().ToList();
+                contacts = conn.Table<Contact>().ToList();
+            }
+
+            if(contacts != null)
+            {
+                //foreach(var c in contacts)
+                //{
+                //    contactsListView.Items.Add(new ListViewItem()
+                //    {
+                //        Content = c
+                //    });
+                //}
+
+                //way better faster to add listview from contact to x:name xaml
+                contactsListView.ItemsSource = contacts;
             }
         }
     }
